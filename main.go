26c729efc34c4a5caaeea5e824af3f1e009975d5
nifle3 <kuper116rus.2005@gmail.com>
2024-02-20 22:53:16 +0300

    add: write byte file

--- main.go
+++ main.go
@@ -11,4 +11,25 @@ func main() {
 		os.Exit(1)
 	}
 
+	if len(os.Args) == 2 {
+	} else if len(os.Args) == 3 {
+		if os.Args[1] != "-c" {
+			fmt.Printf("Usage: %s [-c] <file>\n", os.Args[0])
+			os.Exit(1)
+		}
+
+		file, err := os.Open(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		defer file.Close()
+		statFile, err := file.Stat()
+		fmt.Println(statFile.Size())
+
+	} else {
+		fmt.Printf("Usage: %s [-command] <file>\n", os.Args[0])
+		os.Exit(1)
+	}
 }
